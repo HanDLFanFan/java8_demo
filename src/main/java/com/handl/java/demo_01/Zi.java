f90185892dcbe963df7b763870545a7e77d03d46
hdl <handl_bj@163.com>
2019-05-12 01:14:47 +0800

    init

--- src/main/java/com/handl/java/demo_01/Zi.java
+++
@@ -1,7 +0,0 @@
-package com.handl.java.demo_01;
-
-/**
- * Created by handl on 2017/10/1.
- */
-public class Zi extends Fu {
-}
---
+++ src/main/java/com/handl/java/java8/Stream.java
@@ -0,0 +1,11 @@
+package com.handl.java.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stream {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+    }
+}
